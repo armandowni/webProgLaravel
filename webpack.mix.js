d49f8cfe338eb2e3debbf7f5e435db08029fc176
Armando Nurimansyah <armando.nurimansyah@gmail.com>
2026-01-02 21:37:02 +0700

    fixing css

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,8 @@ const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css', [
+       require('postcss-import'),
        require('tailwindcss'),
-   ]);
+       require('autoprefixer'),
+   ])
+   .version();
