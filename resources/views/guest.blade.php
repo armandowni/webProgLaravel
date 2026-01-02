9ef8a5cbfaefd03358aa358814f52dea5e503019
Armando Nurimansyah <armando.nurimansyah@gmail.com>
2026-01-02 22:27:18 +0700

    fixing css

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -2,60 +2,58 @@
 
 @section('title', 'Home')
 @section('content')
-<div id="containerbarang">
-		<div class="left">
-			<div id="Welcome">
-				<span>Welcome</span>
-				<span>{{$user}},</span>
-			</div>
-			<div id="Mimi_Figure_Shop">
-					<span>Mimi Figure<br/>Shop</span>
-				</div>
-			<div id="Current_time">
-				<span><div id="txt"></div>
-                    </span>
-			</div>
-			<div id="search" class="border-2 border-gray-300 rounded-lg">
-				<form action="home" method="post">
-					@csrf
-					<input type="text" style="width: 150px" name="search">
-					<button name="searchBtn" value="" style="width:30px;"><i class="fa fa-search"></i></button>
-				</form>
-			</div>
-		</div>
+    <div id="containerbarang">
+        <div class="left">
+            <div id="Welcome">
+                <span>Welcome</span>
+                <span>{{ $user }},</span>
+            </div>
+            <div id="Mimi_Figure_Shop">
+                <span>Mimi Figure<br />Shop</span>
+            </div>
+            <div id="Current_time">
+                <span>
+                    <div id="txt"></div>
+                </span>
+            </div>
+            <div id="search">
+                <form action="home" method="post" class="border-2 border-gray-300 rounded-lg">
+                    @csrf
+                    <input type="text" style="width: 150px" name="search">
+                    <button name="searchBtn" value="" style="width:30px;"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+        </div>
 
-		<div id="rowBarang" class="grid grid-cols-3">
-            @foreach($products as $p)
-			<div class="column">
-				<div class="card">
-						<img src="storage/{{$p->product_image}}" alt="Tidak dapat memuat">
-						<div id="isi">
-							<a href="/detail/{{$p->id_product}}">{{$p->product_name}}</a><br><br>
-							<span>{{str_limit($p->product_desc,15,' ...')}}</span><br><br>
-							Rp.<span>{{$p->product_price}}</span>
-							|| Qty. <span>{{$p->product_quantity}}</span>
-							<br>
-						</div>
-						@if(Session::get('role') == "User")
-						<div id="bawah">
-							<h6>{{$p->categories->category_name}}</h6><br>
-							<form action="/home" method="post" >
-								@csrf
-							<button type="submit" name="id_value" value="{{$p->id_product}}">Add to cart</button>
-							</form>
-						</div>
-						@else
-						<div id="bawah">
-							<h5>{{$p->categories->category_name}}</h5>
-						</div>
-						@endif
-
-				</div>
-			</div>
-		@endforeach
-		<div class="pagination">
-				{{ $products->links() }}
-		</div>
-	</div>
-</div>
+        <div class="grid grid-cols-3">
+            @foreach ($products as $p)
+                <div class="card">
+                    <img src="storage/{{ $p->product_image }}" alt="Tidak dapat memuat">
+                    <div id="isi">
+                        <a href="/detail/{{ $p->id_product }}">{{ $p->product_name }}</a><br><br>
+                        <span>{{ str_limit($p->product_desc, 15, ' ...') }}</span><br><br>
+                        Rp.<span>{{ $p->product_price }}</span>
+                        || Qty. <span>{{ $p->product_quantity }}</span>
+                        <br>
+                    </div>
+                    @if (Session::get('role') == 'User')
+                        <div id="bawah">
+                            <h6>{{ $p->categories->category_name }}</h6><br>
+                            <form action="/home" method="post">
+                                @csrf
+                                <button type="submit" name="id_value" value="{{ $p->id_product }}">Add to cart</button>
+                            </form>
+                        </div>
+                    @else
+                        <div id="bawah">
+                            <h5>{{ $p->categories->category_name }}</h5>
+                        </div>
+                    @endif
+                </div>
+            @endforeach
+            <div class="pagination">
+                {{ $products->links() }}
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,80 +1,85 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script>
-		function startTime() {
-			var today = new Date();
-			var h = today.getHours();
-			var m = today.getMinutes();
-			var s = today.getSeconds();
-			m = checkTime(m);
-			s = checkTime(s);
-			document.getElementById('txt').innerHTML =
-					h + ":" + m + ":" + s;
-			var t = setTimeout(startTime, 500);
-		}
-		function checkTime(i) {
-			if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-			return i;
-		}
-	</script>
+    <script>
+        function startTime() {
+            var today = new Date();
+            var h = today.getHours();
+            var m = today.getMinutes();
+            var s = today.getSeconds();
+            m = checkTime(m);
+            s = checkTime(s);
+            document.getElementById('txt').innerHTML =
+                h + ":" + m + ":" + s;
+            var t = setTimeout(startTime, 500);
+        }
+
+        function checkTime(i) {
+            if (i < 10) {
+                i = "0" + i
+            }; // add zero in front of numbers < 10
+            return i;
+        }
+    </script>
 </head>
+
 <body onload="startTime()">
 
-<div id="Login">
-		<div id="Header" class="bg-blue-500 text-white">
-			<div id="login_regis" class="flex justify-between">
-				<div id="Mimi_Shop">
-					<a href="/home">MimiShop</a>
-				</div>
-			@if(Session::get('role') == "User")
-				<ul id="user" class="flex items-center gap-5">
-					<li><a href="/feedbackUser">Feedback</a></li>
-					<li><a href="/cart">My cart</a></li>
-					<li><a href="/transaction">My Transaction</a></li>
-					<li><a href="/profile">Profile</a></li>
-					<li><a href="/signout">Logout</a></li>
-				</ul>
-			@elseif (Session::get('role') == "Admin")
-					<ul id="admin" class="flex items-center gap-5">
-						<li><a href="/manageFeedback">Manage Feedback</a></li>
-						<li><a href="/manageuser">Manage User</a></li>
-						<li><a href="/managefigure">Manage Figure</a></li>
-						<li><a href="/managecategory">Manage Category</a></li>
-						<li><a href="/transactionadmin">Transaction</a></li>
-						<li><a href="/profile">Profile</a></li>
-						<li><a href="/signout">Logout</a></li>
-					</ul>
-				@else
-				@if($_SERVER['REQUEST_URI'] != '/register' && $_SERVER['REQUEST_URI'] != '/signin')
-
-					<div id="Login_A0_Text_7">
-						<a href="/signin">Login</a>
-					</div>
-					<div id="Register">
-						<a href="/register">Register</a>
-					</div>
-					<div id="_">
-						<span>|</span>
-					</div>
-					 @endif
-			@endif
-			</div>
+    <div id="Login">
+        <div id="Header" class="bg-blue-500 text-white">
+            <div id="login_regis" class="flex justify-between">
+                <div id="Mimi_Shop">
+                    <a href="/home">MimiShop</a>
+                </div>
+                @if (Session::get('role') == 'User')
+                    <ul id="user" class="flex items-center gap-5">
+                        <li><a href="/feedbackUser">Feedback</a></li>
+                        <li><a href="/cart">My cart</a></li>
+                        <li><a href="/transaction">My Transaction</a></li>
+                        <li><a href="/profile">Profile</a></li>
+                        <li><a href="/signout">Logout</a></li>
+                    </ul>
+                @elseif (Session::get('role') == 'Admin')
+                    <ul id="admin" class="flex items-center gap-5">
+                        <li><a href="/manageFeedback">Manage Feedback</a></li>
+                        <li><a href="/manageuser">Manage User</a></li>
+                        <li><a href="/managefigure">Manage Figure</a></li>
+                        <li><a href="/managecategory">Manage Category</a></li>
+                        <li><a href="/transactionadmin">Transaction</a></li>
+                        <li><a href="/profile">Profile</a></li>
+                        <li><a href="/signout">Logout</a></li>
+                    </ul>
+                @else
+                    @if ($_SERVER['REQUEST_URI'] != '/register' && $_SERVER['REQUEST_URI'] != '/signin')
+                        <div id="Login_A0_Text_7">
+                            <a href="/signin">Login</a>
+                        </div>
+                        <div id="Register">
+                            <a href="/register">Register</a>
+                        </div>
+                        <div id="_">
+                            <span>|</span>
+                        </div>
+                    @endif
+                @endif
+            </div>
         </div>
 
-		@yield('content')
+        @yield('content')
 
-		<div id="Footer" style="bottom: 0%">
-			<div id="ID2019_Copyright__Bluejack_17_">
-				<span>2019 Copyright: Bluejack 17-2</span>
-			</div>
-		</div>
-</div>
+        <div id="Footer" style="bottom: 0%">
+            <div id="ID2019_Copyright__Bluejack_17_">
+                <span>2019 Copyright: Bluejack 17-2</span>
+            </div>
+        </div>
+    </div>
 
 </body>
+
 </html>
