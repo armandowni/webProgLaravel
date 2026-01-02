4cf0f9e90476c8451c7fc7b3df494cfae596849e
Armando Nurimansyah <armando.nurimansyah@gmail.com>
2026-01-02 20:39:30 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,7 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Fix for older MySQL versions
         Schema::defaultStringLength(191);
+
+        // Force HTTPS for all URLs if not in local
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
